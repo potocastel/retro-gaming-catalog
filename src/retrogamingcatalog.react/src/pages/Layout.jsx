28967c2ed23a45d13e9522010d5486ed887c9cfe
Nicolas Castellano <nicolas.castellano@hotmail.com>
2023-11-29 17:47:13 +0100

    fix layout

--- src/retrogamingcatalog.react/src/pages/Layout.jsx
+++ src/retrogamingcatalog.react/src/pages/Layout.jsx
@@ -3,8 +3,8 @@ import { Outlet, NavLink } from "react-router-dom";
 const Layout = () => {
     return (
         <>
-            <nav class="navbar navbar-light bg-light p-2">
-                <span class="navbar-brand mb-0 h1">Retro gaming catalog</span>
+            <nav className="navbar navbar-light bg-light p-2">
+                <span className="navbar-brand mb-0 h1">Retro gaming catalog</span>
             </nav>            <div className="d-flex flex-row mb-auto">
                 <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark sidebar">
                     <ul className="nav nav-pills flex-column mb-auto">
@@ -14,17 +14,13 @@ const Layout = () => {
                         <li className="nav-item ">
                             <NavLink className="nav-link text-white" to="/games"><i className="bi bi-controller"></i> Games</NavLink>
                         </li>
+                        <li className="nav-item ">
+                            <NavLink className="nav-link text-white" to="/manufacturers"><i className="bi bi-wrench"></i> Manufacturers</NavLink>
+                        </li>
                     </ul>
                 </div>
-                <div className="flex-grow-1">
-                    <div className="d-flex flex-column">
-                        <div className="d-flex flex-row-reverse bg-secondary">
-
-                        </div>
-                        <div className="p-4">
-                            <Outlet />
-                        </div>
-                    </div>
+                <div className="flex-grow-1 p-4">
+                    <Outlet />
                 </div>
             </div>
         </>
